3cf59f10d6f99c868fc218aa4cc9d5b5936c82cd
Angelina <angelinasen@mail.ru>
2021-05-18 23:40:55 +0500

    Поправки

--- SOM/src/js/styler.js
+++ SOM/src/js/styler.js
@@ -1,6 +1,6 @@
   $(function() {
   
-    $('.header__top-currency-select, .header__top-currency-option').styler({
+    $('select, option').styler({
       selectVisibleOptions: 4,
       selectPlaceholder: "USD",
     });
