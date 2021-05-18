809e61d3e4c870789c0b8c79cb5859c83e0b3818
Angelina <angelinasen@mail.ru>
2021-05-18 23:31:21 +0500

    Выпадающие меню

--- SOM/src/js/dropdown.js
+++ SOM/src/js/dropdown.js
@@ -1,56 +1,14 @@
-
-
 $(function () {
-  $('.--clickable').click(function (event) {
+  $('.--clickable > a').click(function (event) {
     event.preventDefault();
-    $(this).children('ul').toggleClass('--active');
+    $(this).next('ul').toggleClass('--active');
+    $(this).toggleClass('--selected');
   });
 
   $(document).click(function (event) {
     if ($(event.target).closest('.--clickable > ul, .--clickable').length) return;
-    $('.--clickable > ul').removeClass('--active')
+    $('.--clickable > ul').removeClass('--active');
+    $('.--clickable > a').removeClass('--selected');
   });
 });
 
-
-/*
-let menuArrows = document.querySelectorAll('.--clickable');
-if (menuArrows.length > 0) {
-  for (let index = 0; index < menuArrows.length; index++) {
-    const menuArrow = menuArrows[index];
-    menuArrow.addEventListener("click", function (e) {
-      event.preventDefault();
-      menuArrow.children[1].classList.toggle('--active');
-    });
-  }
-}
-
-$(function () {
-  $('.header__top-profile').click(function (event) {
-    event.preventDefault();
-    $(this).children('ul').toggleClass('--active');
-  });
-
-  $(document).click(function (event) {
-    if ($(event.target).closest('.header__top-profile-list, .header__top-profile').length) return;
-    $('.header__top-profile-list').removeClass('--active')
-  });
-});
-
-/*
-$(function () {
-  $('.--clickable').click(function (event) {
-    event.preventDefault();
-    $(this).children('ul').addClass('--active');
-    console.log($('.--active').closest('li'));
-    });
-  });
-
-$(function () {
-  $('.--active').closest().find('li').click(function (event) {
-      event.preventDefault();
-      $(this).removeClass('--active');
-    });
-  });
-
-*/
