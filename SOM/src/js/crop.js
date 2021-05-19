12c9207f310dc0af62b0d5ceb7a2e95e39f67b83
Angelina <angelinasen@mail.ru>
2021-05-19 20:52:50 +0500

    scss fix

--- SOM/src/js/crop.js
+++ SOM/src/js/crop.js
@@ -1,4 +1,5 @@
-  let cropImage = [
+$(function () {
+  const cropImage = [
     'polygon(0 0, 100% 0%, 97% 100%, 0% 100%)',
     'polygon(0 0, 100% 0%, 100% 100%, 0 97%)',
     'polygon(0 0, 100% 0%, 100% 97%, 0 100%)',
@@ -8,15 +9,13 @@
     'polygon(5% 0, 100% 0, 100% 100%, 0 100%)',
     'polygon(0 0, 100% 0, 100% 100%, 4% 100%)',
     'polygon(0 0, 98% 0, 100% 100%, 0% 100%)',
-];
+  ];
 
-let images = document.querySelectorAll('.products__item-image');
-console.log(Math.floor(Math.random() * cropImage.length))
-document.addEventListener("DOMContentLoaded", function () {
+  const images = $('.js-crop-image');
+
   if (images.length > 0) {
     for (let index = 0; index < images.length; index++) {
       images[index].style.clipPath = cropImage[Math.floor(Math.random() * cropImage.length)];
     }
-  }
+  };
 });
-  
