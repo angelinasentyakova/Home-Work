88e9687a9e4940493b64b93839f28b8699a9cc50
Angelina <angelinasen@mail.ru>
2021-06-07 17:40:12 +0500

    Адаптив планшет

--- Macarons/src/js/components/Swiper.js
+++ Macarons/src/js/components/Swiper.js
@@ -1,24 +1,57 @@
-let slider = new Swiper('.js-swiper', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 4,
-  speed: 1000,
+const slider = new Swiper('.js-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  speed: 1000,  
 
   pagination: {
     el: '.specials__pagination',
     clickable: true,
   },
+
+  breakpoints: {
+    
+    560: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    990: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    }
+    
+  }
 });
 
-let mySwiper = new Swiper('.js-swiper-2', {
-    slidesPerView: 4,
-    spaceBetween: 9,
-    slidesPerGroup: 4,
+const slider2 = new Swiper('.js-swiper-2', {
+    slidesPerView: 1,
+    spaceBetween: 10,
     speed: 1000,
 
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+  },
+    
+  breakpoints: {
+    
+    560: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
+    990: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 9,
+    }
+    
+  }
+    
 
   });
